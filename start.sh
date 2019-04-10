b564020eb2600e252127cfa3753b6d26674e8182
Han Qiao <qiao@basis-ai.com>
2019-04-10 14:40:20 +0800

    Update api token request header

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -euo pipefail
 
-result=$(curl -sL ${SRC_URL} | curl -sL -H "Authorization: Bearer ${API_KEY}" --data-binary @- ${DST_URL})
+result=$(curl -sL ${SRC_URL} | curl -sL -H "X-Bedrock-Api-Token: ${API_KEY}" --data-binary @- ${DST_URL})
 
 if [ ! -z "$result" ]
 then

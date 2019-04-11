4c68dfc8d34beb7fb4a7ba693822bb056da802be
Han Qiao <qiao@basis-ai.com>
2019-04-11 09:59:27 +0800

    Push metrics periodically

--- start.sh
+++ start.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env sh
 set -euo pipefail
 
-result=$(curl -sL ${SRC_URL} | curl -sL -H "X-Bedrock-Api-Token: ${API_KEY}" --data-binary @- ${DST_URL})
+while true
+do
+    result=$(curl -sL ${SRC_URL} | curl -sL -H "X-Bedrock-Api-Token: ${API_KEY}" --data-binary @- ${DST_URL})
+    if [ ! -z "$result" ]
+    then
+        echo $result
+        # exit 1
+    fi
 
-if [ ! -z "$result" ]
-then
-    echo $result
-    exit 1
-fi
+    sleep ${PUSH_INTERVAL}
+done
